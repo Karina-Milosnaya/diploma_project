3ccfe2dc12d6f09a048ac915d2de34e7b4f7343d
Karina-Milosnaya <k.milosnaya@gmail.com>
2024-12-02 23:54:58 +0400

    add Title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
     <>
-      <Header fullname="Artem Malkin"></Header>
+      {/* <Header fullname="Artem Malkin"></Header> */}
+      <Title></Title>
     </>
   );
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+function Title() {
+  return (
+    <>
+      <h1 className="main-title">Blog</h1>
+    </>
+  );
+}
+
+export default Title;
